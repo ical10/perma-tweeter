065ff96fe5c8266bf664245a2795fb780dc5a32a
ical10 <mochhusnir@gmail.com>
2022-11-16 13:53:51 +0700

    feat: create http POST call to fetch tweet by tweetId

--- pages/crosspost.tsx
+++ pages/crosspost.tsx
@@ -1,5 +1,6 @@
 import type { NextPage, GetServerSidePropsContext } from "next";
 import dynamic from "next/dynamic";
+import React, { useState } from "react";
 import Head from "next/head";
 import { authOptions } from "pages/api/auth/[...nextauth]";
 import FullScreenLoading from "src/components/FullScreenLoading";
@@ -7,24 +8,19 @@ import { useRouter } from "next/router";
 import { useSession } from "next-auth/react";
 import AuthButton from "src/components/Button";
 import { unstable_getServerSession } from "next-auth/next";
-import Image from "next/image";
 import { useSubSocialApiHook } from "src/hooks/use-subsocial-api";
-import { TweetsProps } from "src/types/common";
+import { TweetProps } from "src/types/common";
 const Appbar = dynamic(() => import("src/components/Appbar"), {
   ssr: false,
 });
 
-const TweetPage: NextPage<TweetsProps> = () => {
+const CrossPostPage: NextPage<TweetProps> = () => {
   const { data: session, status } = useSession();
   const router = useRouter();
   const { initApi, loading, postTransaction } = useSubSocialApiHook();
-  //const [savedPosts, setSavedPosts] = useState<PostProps[]>([]);
 
-  //useEffect(() => {
-  //if (savedPosts.length > 0 && session) {
-  //initApi({ mnemonic: session.mnemonic });
-  //}
-  //}, [savedPosts.length, session]);
+  const [tweetUrl, setTweetUrl] = useState("");
+  const [fetchedTweet, setFetchedTweet] = useState<TweetProps | null>(null);
 
   if (status === "loading") return <FullScreenLoading />;
 
@@ -43,6 +39,27 @@ const TweetPage: NextPage<TweetsProps> = () => {
 
   if (!session) return null;
 
+  const handleSignAnSubmit = async () => {
+    const { token } = session;
+    const tweetId = tweetUrl.split("/")[5];
+
+    const response = await fetch("/api/crosspost", {
+      method: "POST",
+      body: JSON.stringify({ tweetId, token }),
+      headers: {
+        "Content-type": "application/json",
+      },
+    });
+
+    const data = await response.json();
+
+    setFetchedTweet(data);
+  };
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setTweetUrl(event.target.value);
+  };
+
   return (
     <>
       <Head>
@@ -57,8 +74,38 @@ const TweetPage: NextPage<TweetsProps> = () => {
             <AuthButton text={"Logout"} />
             <a>{`Welcome! You are logged in as @${session.user.name}`}</a>
           </div>
-          <div className="flex flex-row max-h-screen p-4">
-            <div className="flex flex-col overflow-y-auto overflow-x-hidden"></div>
+          <div>
+            <h2>Cross-post a tweet into Subsocial</h2>
+            {fetchedTweet ? (
+              <div
+                key={fetchedTweet.id}
+                className="p-6 max-w-lg bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-blue-500 dark:hover:bg-blue-500
+                flex flex-col items-center mb-4"
+              >
+                <div className="flex flex-col items-start py-2 px-4">
+                  {fetchedTweet.text}
+                </div>
+              </div>
+            ) : (
+              <></>
+            )}
+            <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+              Tweet URL
+            </label>
+            <input
+              type="text"
+              id="tweet-url"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              placeholder="https://twitter.com/cat_auras/status/1592442883424276486"
+              value={tweetUrl}
+              onChange={handleChange}
+              required
+            />
+            {fetchedTweet ? (
+              <button>Send post to Subsocial</button>
+            ) : (
+              <button onClick={handleSignAnSubmit}>Find tweet</button>
+            )}
           </div>
         </div>
       </Appbar>
@@ -83,4 +130,4 @@ export async function getServerSideProps(ctx: GetServerSidePropsContext) {
   };
 }
 
-export default TweetPage;
+export default CrossPostPage;
