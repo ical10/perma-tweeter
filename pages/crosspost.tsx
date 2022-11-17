75d18acf7819a7772ea39ddfb7efacee497b06da
ical10 <mochhusnir@gmail.com>
2022-11-17 15:46:12 +0700

    feat: create find tweet and clear url functionalities

--- pages/crosspost.tsx
+++ pages/crosspost.tsx
@@ -2,6 +2,7 @@ import type { NextPage, GetServerSidePropsContext } from "next";
 import dynamic from "next/dynamic";
 import React, { useState } from "react";
 import Head from "next/head";
+import Image from "next/image";
 import { authOptions } from "pages/api/auth/[...nextauth]";
 import FullScreenLoading from "src/components/FullScreenLoading";
 import { useRouter } from "next/router";
@@ -9,19 +10,21 @@ import { useSession } from "next-auth/react";
 import AuthButton from "src/components/Button";
 import { unstable_getServerSession } from "next-auth/next";
 import { useSubSocialApiHook } from "src/hooks/use-subsocial-api";
-import { TweetProps } from "src/types/common";
+import { TweetWithAuthorProps } from "src/types/common";
 const Appbar = dynamic(() => import("src/components/Appbar"), {
   ssr: false,
 });
 
-const CrossPostPage: NextPage<TweetProps> = () => {
+const CrossPostPage: NextPage = () => {
   const { data: session, status } = useSession();
   const router = useRouter();
   const { initApi, loading, postTransaction } = useSubSocialApiHook();
 
   const [tweetUrl, setTweetUrl] = useState("");
   const [loadingTweet, setLoadingTweet] = useState(false);
-  const [fetchedTweet, setFetchedTweet] = useState<TweetProps | null>(null);
+  const [fetchedTweet, setFetchedTweet] = useState<TweetWithAuthorProps | null>(
+    null
+  );
 
   if (status === "loading") return <FullScreenLoading />;
 
@@ -40,7 +43,7 @@ const CrossPostPage: NextPage<TweetProps> = () => {
 
   if (!session) return null;
 
-  const handleSignAnSubmit = async () => {
+  const handleFetchTweet = async () => {
     setLoadingTweet(true);
 
     try {
@@ -55,9 +58,20 @@ const CrossPostPage: NextPage<TweetProps> = () => {
         },
       });
 
-      const data = await response.json();
+      const { data, includes } = await response.json();
 
-      setFetchedTweet(data);
+      const { author_id, edit_history_tweet_ids, id, text } = data;
+      const { users } = includes;
+
+      const payload = {
+        author_id,
+        edit_history_tweet_ids,
+        id,
+        text,
+        users,
+      };
+
+      setFetchedTweet(payload);
     } catch (error) {
       console.warn({ error });
     } finally {
@@ -69,6 +83,18 @@ const CrossPostPage: NextPage<TweetProps> = () => {
     setTweetUrl(event.target.value);
   };
 
+  const handleClearUrl = () => {
+    setTweetUrl("");
+  };
+
+  const getAuthor = (tweet: TweetWithAuthorProps) => {
+    const temp = tweet.users?.find((user) => user.id === tweet.author_id);
+
+    return {
+      temp,
+    };
+  };
+
   return (
     <>
       <Head>
@@ -78,12 +104,13 @@ const CrossPostPage: NextPage<TweetProps> = () => {
       </Head>
 
       <Appbar>
-        <div className="flex flex-row items-center justify-center max-w-full max-h-screen">
-          <div className="flex flex-col self-start mt-4 p-4 gap-2 max-w-[500px]">
+        <div className="grid grid-cols-[0.75fr_1.75fr_0.75fr] max-w-full max-h-screen">
+          <div className="flex flex-col self-start mt-4 p-4 gap-2">
             <AuthButton text={"Logout"} />
             <a>{`Welcome! You are logged in as @${session.user.name}`}</a>
           </div>
-          <div>
+
+          <div className="flex flex-col max-h-screen p-4">
             <h2>Cross-post a tweet into Subsocial</h2>
             {fetchedTweet ? (
               <div
@@ -91,6 +118,24 @@ const CrossPostPage: NextPage<TweetProps> = () => {
                 className="p-6 max-w-lg bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-blue-500 dark:hover:bg-blue-500
                 flex flex-col items-center mb-4"
               >
+                <div className="flex flex-row items-center self-start justify-center gap-2">
+                  <Image
+                    src={getAuthor(fetchedTweet).temp?.profile_image_url ?? ""}
+                    alt="user-avatar"
+                    className="rounded-full"
+                    width="32"
+                    height="32"
+                  />
+                  <div>
+                    <div>
+                      {getAuthor(fetchedTweet).temp?.name ?? "Twitter user"}
+                    </div>
+                    <div>
+                      {`@${getAuthor(fetchedTweet).temp?.username}` ??
+                        "@username"}
+                    </div>
+                  </div>
+                </div>
                 <div className="flex flex-col items-start py-2 px-4">
                   {fetchedTweet.text}
                 </div>
@@ -110,13 +155,36 @@ const CrossPostPage: NextPage<TweetProps> = () => {
               onChange={handleChange}
               required
             />
-            {fetchedTweet ? (
-              <button>Send post to Subsocial</button>
-            ) : (
-              <button disabled={loadingTweet} onClick={handleSignAnSubmit}>
+            <div>
+              <button
+                className="disabled:bg-gray-300 disabled:hover:bg-gray-100 text-red font-bold py-2 px-4 rounded"
+                disabled={!tweetUrl}
+                onClick={handleClearUrl}
+              >
+                Clear URL
+              </button>
+              <button
+                className="bg-blue-500 disabled:bg-gray-300 disabled:hover:bg-gray-100 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                disabled={loadingTweet || !tweetUrl}
+                onClick={handleFetchTweet}
+              >
                 {loadingTweet ? "Fetching tweet..." : "Find tweet"}
               </button>
-            )}
+            </div>
+          </div>
+
+          <div>Space owned by: {}</div>
+
+          <div className="flex flex-col self-start items-center justify-center mt-4 p-4 gap-2">
+            <button
+              className="bg-blue-500 disabled:bg-gray-300 disabled:hover:bg-gray-100 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+              disabled={!fetchedTweet}
+              //onClick={handlePostTransaction}
+            >
+              Send post to Subsocial!
+            </button>
+            <a>{loading ? "Sending tx, open your console" : ""}</a>
+            <a>Find tweet first to be sent</a>
           </div>
         </div>
       </Appbar>
